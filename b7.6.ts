f8ed360c3e3ed5864e6cca759b3605fe13f2ee11
Linhtranne <147390264+Linhtranne@users.noreply.github.com>
2024-04-09 15:45:56 +0700

    Update b7.6.ts

--- b7.6.ts
+++ b7.6.ts
@@ -1,10 +1,1 @@
-let numbers: number[] = [1, 2, 3, 4, 5];
-let person: [string, number] = ["Tran Quang Linh", 18];
-numbers.push(6);
-numbers[0] = "a";
-let firstNumber: number = numbers[0];
-let firstName: string = person[0];
-let [name, age] = person;
-let allNumbers: number[] = [...numbers, 6, 7];
-
-console.log(numbers, person);
+he
