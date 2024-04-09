1e3c9fc461bb70437db599d4a9396cffd545b11b
Linhtranne <147390264+Linhtranne@users.noreply.github.com>
2024-04-09 15:37:07 +0700

    Update b7.7.ts

--- b7.7.ts
+++ b7.7.ts
@@ -2,7 +2,7 @@ const students: any[] = [
     { id: 1, name: "he he he he he", class: "S" },
     { id: 2, name: "hehehe", class: "S" },
     { id: 3, name: "he he he he he", class: "C" },
-    { id: 4, name: "he he he he, class: C2" },
+    { id: 4, name: "he he he he, class: C" },
   ];
   function getStudentsByClass(students: any[], className: string): any[] {
     const filteredStudents: any[] = [];
@@ -14,5 +14,5 @@ const students: any[] = [
     return filteredStudents;
   }
 
-  const studentsInA1: any[] = getStudentsByClass(students, "A1");
+  const studentsInA1: any[] = getStudentsByClass(students, "S");
   console.log(studentsInA1);
