19cf949f00f3b9350b9923ff0b1b153493381536
Linhtranne <147390264+Linhtranne@users.noreply.github.com>
2024-04-09 13:32:48 +0700

    Update b7.10.ts

--- b7.10.ts
+++ b7.10.ts
@@ -1,56 +1,21 @@
-// Hàm cộng
-function add(a: number | string, b: number | string): number {
-    let num1: number = toNumber(a);
-    let num2: number = toNumber(b);
-  
-    if (isNaN(num1) || isNaN(num2)) {
-      throw new Error("Tham số không hợp lệ");
+function removeProperties(obj: any): any {
+  const result: any = {};
+  for (const key in obj) {
+    if (obj[key]) {
+      result[key] = obj[key];
     }
-  
-    return num1 + num2;
   }
-  
-  // Hàm trừ
-  function subtract(a: number | string, b: number | string): number {
-    let num1: number = toNumber(a);
-    let num2: number = toNumber(b);
-  
-    if (isNaN(num1) || isNaN(num2)) {
-      throw new Error("Tham số không hợp lệ");
-    }
-  
-    return num1 - num2;
-  }
-  
-  // Hàm nhân
-  function multiply(a: number | string, b: number | string): number {
-    let num1: number = toNumber(a);
-    let num2: number = toNumber(b);
-  
-    if (isNaN(num1) || isNaN(num2)) {
-      throw new Error("Tham số không hợp lệ");
-    }
-  
-    return num1 * num2;
-  }
-  
-  // Hàm chia
-  function divide(a: number | string, b: number | string): number {
-    let num1: number = toNumber(a);
-    let num2: number = toNumber(b);
-  
-    if (isNaN(num1) || isNaN(num2)) {
-      throw new Error("Tham số không hợp lệ");
-    }
-  
-    return num1 / num2;
-  }
-  
-  // Hàm chuyển đổi chuỗi sang số
-  function toNumber(value: string): number {
-    let num: number = Number(value);
-    if (isNaN(num)) {
-      throw new Error("Chuỗi không thể chuyển đổi sang số");
-    }
-    return num;
-  }
+  return result;
+}
+const input = {
+  a: null,
+  b: false,
+  c: true,
+  d: 0,
+  e: 1,
+  f: "",
+  g: "a",
+};
+
+const output = removeProperties(input);
+console.log(output);
